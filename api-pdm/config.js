b32421c066588441a757f5a5ceff33da8c149398
00005016@uca.edu.sv <john200_@hotmail.com>
2018-07-10 17:18:47 -0500

    primero

---
+++ api-pdm/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    port:process.env.PORT || 3000,
+    db:process.env.MONGODB || 'mongodb://erika:rootuca2018@ds241875.mlab.com:41875/parcialfinalpdm',
+    SECRET_TOKEN:'tokensecretopdm_2018',
+    image_url:'https://proyectopdm2018.herokuapp.com/raulcampost/ProyectoPDM/tree/master/api-pdm/uploads/'
+};
